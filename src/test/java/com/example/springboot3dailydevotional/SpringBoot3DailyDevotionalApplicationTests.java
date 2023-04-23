8a90f249be08aaa4afb22962ac1b88ecde785f48
Erik Yuzwa <erikyuzwa@gmail.com>
2023-04-23 14:12:24 -0600

    initial commit

---
+++ src/test/java/com/example/springboot3dailydevotional/SpringBoot3DailyDevotionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3dailydevotional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3DailyDevotionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
